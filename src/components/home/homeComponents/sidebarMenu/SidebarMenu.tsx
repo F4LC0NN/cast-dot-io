d01f9f1c7b5a870f8b45f0cf77cfc9305c82ddb9
F4LC0NN <thomasruben25@gmail.com>
2022-05-29 11:28:15 +0200

    Remove tailwindcss module & install sass module

--- src/components/home/homeComponents/sidebarMenu/SidebarMenu.tsx
+++ src/components/home/homeComponents/sidebarMenu/SidebarMenu.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import burgerMenuIcon from '../../../../assets/images/burgerMenu.svg';
+
+import house from '../../../../assets/images/house.svg';
+import lens from '../../../../assets/images/lens.svg';
+import cast from '../../../../assets/images/cast.svg';
+import about from '../../../../assets/images/about.svg';
+import cog from '../../../../assets/images/cog.svg';
 
 function SidebarMenu(): JSX.Element {
+  const [isToggle, setIsToggle] = useState(true);
+
+  function toggleNavBar(): void {
+    if (isToggle) {
+      setIsToggle(false);
+    } else {
+      setIsToggle(true);
+    }
+  }
+
   return (
     <>
-      <button type="button">*///*</button>
+      <button type="button" onClick={toggleNavBar}>
+        <img src={burgerMenuIcon} alt="Burger Menu" />
+      </button>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>Search city</li>
-          <li>Weather onAir</li>
-          <li>About</li>
-          <li>Settings</li>
+          <li>
+            <img src={house} alt="Home" />
+            <p>Home</p>
+          </li>
+          <li>
+            <img src={lens} alt="Search City" />
+            <p>Search city</p>
+          </li>
+          <li>
+            <img src={cast} alt="Weather News" />
+            <p>Weather News</p>
+          </li>
+          <li>
+            <img src={about} alt="About" />
+            <p>About</p>
+          </li>
+          <li>
+            <img src={cog} alt="Settings" />
+            <p>Settings</p>
+          </li>
         </ul>
+        <button type="button" onClick={toggleNavBar}>
+          Close menu
+        </button>
       </nav>
     </>
   );
